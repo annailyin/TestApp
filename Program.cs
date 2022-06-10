2054dec889ba282b6f0b35c1ae4b31a7c8bc8767
Anna Ilyinkova <anna.ilyin@gmail.com>
2022-06-10 13:36:48 +0300

    Initial project version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a + 10;
+            Console.WriteLine("Sum a + b = {0}", a + b);
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
